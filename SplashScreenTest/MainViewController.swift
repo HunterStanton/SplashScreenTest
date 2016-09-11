1776533f05174f2d84a9228dc0a9b3f16963b7a3
Gregory Hunter Stanton <rbfan@live.com>
2016-09-11 19:40:23 -0400

    Added required view controller classes, made a new group for the view controllers, and also setup the timer in SplashScreenViewController

---
+++ SplashScreenTest/MainViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MainViewController.swift
+//  SplashScreenTest
+//
+//  Created by Hunter Stanton on 9/11/16.
+//  Copyright © 2016 HunterStanton. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController{
+    
+    override func viewDidLoad(){
+        super.viewDidLoad()
+    }
+}
---
+++ SplashScreenTest/SplashScreenViewController.swift
@@ -0,0 +1,32 @@
+//
+//  SplashScreenViewController.swift
+//  SplashScreenTest
+//
+//  Created by Hunter Stanton on 9/11/16.
+//  Copyright © 2016 HunterStanton. All rights reserved.
+//
+
+import UIKit
+
+class SplashScreenViewController: UIViewController{
+    
+    override func viewDidLoad() {
+        // We'll use viewDidLoad to setup our timer and move to the new view whenever the timer is up
+        // Currently the time is set to Pi seconds but can be changed here
+        
+        // Double as opposed to int so we can get precise timings
+        let timer_interval: Double = M_PI
+        
+        // Create our timer and start it instantly
+        // We want no interaction from the user necessary on this screen so we must begin things instantly
+        let timer: NSTimer = NSTimer.scheduledTimerWithTimeInterval(timer_interval, target: self, selector: Selector("timerDidFire"), userInfo: nil, repeats: false)
+        
+        
+        super.viewDidLoad()
+    }
+    
+    func timerDidFire()
+    {
+        print("timer fired")
+    }
+}
